e7d4309d3fd4f9841e581e89178231c3389e13ad
hamza herbou <35342314+herbou@users.noreply.github.com>
2020-01-18 22:21:18 +0100

    Add files via upload

---
+++ Scripts/SceneMg.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneMg : MonoBehaviour
+{
+	[SerializeField] GameObject exitPanel;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (SceneManager.GetActiveScene ().buildIndex != 0) {
+				SceneManager.LoadScene (0);
+			} else {
+				if (exitPanel) {
+					exitPanel.SetActive (true);
+				}
+			}
+		}
+	}
+
+	public void onUserClickYesNo (int choice)
+	{//choice==0 no     choice==1 yes
+		if (choice == 1) {
+			Application.Quit ();
+		}
+		exitPanel.SetActive (false);//else
+	}
+
+	public void onUserClickStart ()
+	{
+		SceneManager.LoadScene (1);//load game scene
+	}
+}
